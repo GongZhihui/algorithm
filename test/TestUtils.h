641d6fdb9e6910bdc5e812b66dcf9ce31685b1b7
gongzhihui <gongzhihui@ecosysnet.com>
2019-03-06 14:01:32 +0800

    .

--- test/TestUtils.h
+++ test/TestUtils.h
@@ -23,26 +23,26 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #define ALGORITHM_TEST_CASE(FUNC) void FUNC()
 
 // FLAG if FLAG is true， then execute FUNC()
-#define ALGORITHM_EXECUTE_CASE(FUNC, FLAG)                     \
-    if (FLAG)                                                  \
-    {                                                          \
-        std::cout << "[" #FUNC << "] === start" << std::endl;  \
-        FUNC();                                                \
-        std::cout << "[" << #FUNC << "] === end" << std::endl; \
-        std::cout << "" << std::endl;                          \
+#define ALGORITHM_EXECUTE_CASE(FUNC, FLAG)                       \
+    if (FLAG)                                                    \
+    {                                                            \
+        std::cout << "[" << #FUNC << "] === start" << std::endl; \
+        FUNC();                                                  \
+        std::cout << "[" << #FUNC << "] === end" << std::endl;   \
+        std::cout << "" << std::endl;                            \
     }
 
 // FLAG if FLAG is true， then execute FUNC()
-#define ALGORITHM_EXECUTE(FUNC, FLAG)                  \
-    if (FLAG)                                          \
-    {                                                  \
-        std::cout << std::endl                         \
-                  << "[" #FUNC << "] **** start ****"  \
-                  << std::endl;                        \
-        FUNC();                                        \
-        std::cout << "[" << #FUNC << "] **** end ****" \
-                  << std::endl;                        \
-        std::cout << "" << std::endl;                  \
+#define ALGORITHM_EXECUTE(FUNC, FLAG)                    \
+    if (FLAG)                                            \
+    {                                                    \
+        std::cout << std::endl                           \
+                  << "[" << #FUNC << "] **** start ****" \
+                  << std::endl;                          \
+        FUNC();                                          \
+        std::cout << "[" << #FUNC << "] **** end ****"   \
+                  << std::endl;                          \
+        std::cout << "" << std::endl;                    \
     }
 
 #endif //!TEST_UTILS_H
